e0f4636c7545c45b5e048666659d4036e90001b3
guskirb <anguskirby1998@gmail.com>
2024-10-08 01:35:25 +0100

    Add header component and styling

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,9 +1,23 @@
+import { Phone } from "lucide-react";
 
 export default function Header() {
   return (
-    <div className="w-full">
-
+    <div className="w-full border border-black h-24 flex justify-between items-center">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="">B.A.S.E.</h1>
+        <h2 className="text-sm">
+          Bournemouth Army Surplus Equipment & Base Kit
+        </h2>
+      </div>
+      <div className="flex items-center gap-8">
+        <div className="flex gap-2">
+          <Phone />
+          <h2>01202 535555</h2>
+        </div>
+        <button>Home</button>
+        <button>About</button>
+        <button>Contact Us</button>
+      </div>
     </div>
-  )
+  );
 }
-
