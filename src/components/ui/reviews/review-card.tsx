d8d30bc4bab976210e5e20ed3ea2ec94339d4023
guskirb <anguskirby1998@gmail.com>
2024-10-10 07:42:25 +0100

    Add links to review card img & name

--- src/components/ui/reviews/review-card.tsx
+++ src/components/ui/reviews/review-card.tsx
@@ -4,13 +4,17 @@ export const ReviewCard = ({ review }: { review: any }) => {
   return (
     <div className="p-8 min-w-[200px] w-[350px] grow flex flex-col gap-5 rounded-xl bg-gray-100">
       <div className="flex items-center gap-5">
-        <img
-          src={review.authorAttribution.photoURI}
-          alt="Reviewer profile image"
-          className="w-20 h-20"
-        />
+        <a href={review.authorAttribution.uri}>
+          <img
+            src={review.authorAttribution.photoURI}
+            alt="Reviewer profile image"
+            className="w-20 h-20"
+          />
+        </a>
         <div className="flex flex-col">
-          <p>{review.authorAttribution.displayName}</p>
+          <a href={review.authorAttribution.uri}>
+            <p className="font-semibold">{review.authorAttribution.displayName}</p>
+          </a>
           <p className="mt-auto opacity-50">{review.Ig}</p>
           <Rating
             initialValue={review.rating}
@@ -27,7 +31,6 @@ export const ReviewCard = ({ review }: { review: any }) => {
           ? review.text.slice(0, 270) + "..."
           : review.text}
       </p>
-      
     </div>
   );
 };
