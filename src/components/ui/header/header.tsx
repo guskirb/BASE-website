626819313647c5cf84da7dd920316c0bc9249a6f
guskirb <anguskirby1998@gmail.com>
2024-10-10 07:39:59 +0100

    Responsive styling

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -5,20 +5,22 @@ import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <div className="w-full 2xl:w-[1400px] h-24 flex justify-between items-center px-8 lg:px-16">
-      <div className="flex flex-col justify-center items-center">
-        <Link to="/">
-          <h1 className="text-lime-700 text-2xl lg:text-4xl">B.A.S.E.</h1>
-        </Link>
-      </div>
-      <div className="flex items-center gap-5 lg:gap-16">
-        <div className="hidden lg:flex gap-2 items-center">
-          <Phone size={18} />
-          <h2 className="font-medium">01202 535555</h2>
+    <div className="w-full flex justify-center bg-black">
+      <div className="w-full 2xl:w-[1400px] h-24 flex justify-between items-center px-8 lg:px-16">
+        <div className="flex flex-col justify-center items-center">
+          <Link to="/">
+            <h1 className="text-white text-2xl lg:text-4xl">B.A.S.E.</h1>
+          </Link>
         </div>
-        <TextButton path="/">HOME</TextButton>
-        <TextButton path="/about">ABOUT</TextButton>
-        <TextButton path="/contact">CONTACT</TextButton>
+        <div className="flex items-center gap-5 lg:gap-16 text-white">
+          <div className="hidden lg:flex gap-2 items-center">
+            <Phone size={18} />
+            <h2 className="font-medium">01202 535555</h2>
+          </div>
+          <TextButton path="/">HOME</TextButton>
+          <TextButton path="/about">ABOUT</TextButton>
+          <TextButton path="/contact">CONTACT</TextButton>
+        </div>
       </div>
     </div>
   );
--- src/components/ui/reviews/review-card.tsx
+++ src/components/ui/reviews/review-card.tsx
@@ -3,13 +3,13 @@ import { Rating } from "react-simple-star-rating";
 export const ReviewCard = ({ review }: { review: any }) => {
   return (
     <div className="p-8 min-w-[200px] w-[350px] grow flex flex-col gap-5 rounded-xl bg-gray-100">
-      <div className="flex flex-col justify-center items-center gap-5">
+      <div className="flex items-center gap-5">
         <img
           src={review.authorAttribution.photoURI}
           alt="Reviewer profile image"
           className="w-20 h-20"
         />
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col">
           <p>{review.authorAttribution.displayName}</p>
           <p className="mt-auto opacity-50">{review.Ig}</p>
           <Rating
@@ -22,9 +22,9 @@ export const ReviewCard = ({ review }: { review: any }) => {
           />
         </div>
       </div>
-      <p className="text-center">
-        {review.text.length > 350
-          ? review.text.slice(0, 350) + "..."
+      <p className="">
+        {review.text.length > 270
+          ? review.text.slice(0, 270) + "..."
           : review.text}
       </p>
       
